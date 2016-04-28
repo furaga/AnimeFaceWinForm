e469593ec15e80a5ec5e7888b7b5148ce071a978
furaga <furaga.fukahori@gmail.com>
2016-04-29 04:50:10 +0900

    first commit

---
+++ AnimeFaceWinForm/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace AnimeFaceWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.outputFolderButton = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.startFrame = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.skipFrames = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.startFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skipFrames)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(237, 189);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(490, 30);
+            this.progressBar.TabIndex = 2;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(737, 194);
+            this.progressLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(54, 21);
+            this.progressLabel.TabIndex = 4;
+            this.progressLabel.Text = "---%";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(13, 185);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 34);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(173, 34);
+            this.pathLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(125, 21);
+            this.pathLabel.TabIndex = 3;
+            this.pathLabel.Text = "Not Selected";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(121, 185);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(107, 34);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // outputFolderButton
+            // 
+            this.outputFolderButton.Enabled = false;
+            this.outputFolderButton.Location = new System.Drawing.Point(13, 273);
+            this.outputFolderButton.Margin = new System.Windows.Forms.Padding(4);
+            this.outputFolderButton.Name = "outputFolderButton";
+            this.outputFolderButton.Size = new System.Drawing.Size(152, 37);
+            this.outputFolderButton.TabIndex = 8;
+            this.outputFolderButton.Text = "Output Folder";
+            this.outputFolderButton.UseVisualStyleBackColor = true;
+            this.outputFolderButton.Click += new System.EventHandler(this.outputFolderButton_Click);
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(13, 27);
+            this.selectButton.Margin = new System.Windows.Forms.Padding(4);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(139, 34);
+            this.selectButton.TabIndex = 9;
+            this.selectButton.Text = "Select Video";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // startFrame
+            // 
+            this.startFrame.Location = new System.Drawing.Point(144, 106);
+            this.startFrame.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.startFrame.Name = "startFrame";
+            this.startFrame.Size = new System.Drawing.Size(92, 28);
+            this.startFrame.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Start Frame";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 21);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Skip Frames";
+            // 
+            // skipFrames
+            // 
+            this.skipFrames.Location = new System.Drawing.Point(142, 145);
+            this.skipFrames.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.skipFrames.Name = "skipFrames";
+            this.skipFrames.Size = new System.Drawing.Size(94, 28);
+            this.skipFrames.TabIndex = 14;
+            this.skipFrames.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 327);
+            this.Controls.Add(this.skipFrames);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startFrame);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.outputFolderButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.progressBar);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "AnimeFace WinForm Frontend";
+            ((System.ComponentModel.ISupportInitialize)(this.startFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skipFrames)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button outputFolderButton;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.NumericUpDown startFrame;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown skipFrames;
+    }
+}
+
---
+++ AnimeFaceWinForm/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimeFaceWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class bgWorkArg
+        {
+            public string filepath;
+            public int startFrame;
+            public int skipFrames;
+        }
+
+        bool selectVideoFile()
+        {
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+            }
+            else
+            {
+                openFileDialog.FileName = "";
+            }
+
+            pathLabel.Text = openFileDialog.FileName;
+
+            return System.IO.File.Exists(openFileDialog.FileName);
+        }
+
+        void setUIEnabled(bool isRunning)
+        {
+            if (isRunning)
+            {
+                // 実行中はキャンセルボタン以外無効
+                selectButton.Enabled = false;
+                startFrame.Enabled = false;
+                skipFrames.Enabled = false;
+                startButton.Enabled = false;
+                cancelButton.Enabled = true;
+            }
+            else
+            {
+                // 実行してなければキャンセルボタン以外有効
+                selectButton.Enabled = true;
+                startFrame.Enabled = true;
+                skipFrames.Enabled = true;
+                startButton.Enabled = true;
+                cancelButton.Enabled = false;
+            }
+
+            string ouputDirPath = outputFolderButton.Tag as string;
+            if (ouputDirPath != null && System.IO.Directory.Exists(ouputDirPath))
+            {
+                outputFolderButton.Enabled = true;
+            }
+            else
+            {
+                outputFolderButton.Enabled = false;
+            }
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            bool result = selectVideoFile();
+            outputFolderButton.Tag = getDirectoryName(openFileDialog.FileName);
+            setUIEnabled(false);
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy == false)
+            {
+                if (false == System.IO.File.Exists(openFileDialog.FileName))
+                {
+                    bool result = selectVideoFile();
+                    if (false == result)
+                    {
+                        return;
+                    }
+                }
+
+                var arg = new bgWorkArg()
+                {
+                    filepath = openFileDialog.FileName,
+                    startFrame = (int)startFrame.Value,
+                    skipFrames = (int)skipFrames.Value,
+                };
+
+                outputFolderButton.Tag = getDirectoryName(arg.filepath);
+                backgroundWorker.RunWorkerAsync(arg);
+                setUIEnabled(true);
+            }
+        }
+
+        string getDirectoryName(string inputVideoFilePath)
+        {
+            string dirName = System.IO.Path.GetFullPath("output/" + System.IO.Path.GetFileNameWithoutExtension(inputVideoFilePath));
+            return dirName;
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = (BackgroundWorker)sender;
+
+            var arg = e.Argument as bgWorkArg;
+            string filepath = arg.filepath;
+            string dirName = getDirectoryName(filepath);
+
+            worker.ReportProgress(0);
+
+            using (var capture = new OpenCvSharp.CvCapture(filepath))
+            {
+                if (capture == null)
+                {
+                    return;
+                }
+
+                if (System.IO.Directory.Exists(dirName) == false)
+                {
+                    System.IO.Directory.CreateDirectory(dirName);
+                }
+
+                System.Diagnostics.Process.Start(dirName);
+                capture.PosFrames = arg.startFrame;
+
+                int prevProgress = -1;
+
+                while (true)
+                {
+                    // キャンセルされてないか定期的にチェック
+                    if (worker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+
+                    // 画像取得→顔認識
+                    using (var cap = capture.RetrieveFrame())
+                    {
+                        if (cap == null)
+                        {
+                            e.Result = true;
+                            return;
+                        }
+
+                        // 画像を一旦保存して、pythonに渡す
+                        cap.SaveImage(System.IO.Path.GetFullPath("../python/input.png"));
+
+                        // python実行
+                        bool result;
+                        string output = RunCPython("../python", "detect.py input.png", out result);
+
+                        if (false == result)
+                        {
+                            e.Result = false;
+                            return;
+                        }
+
+                        // 結果をパースして、得られた顔領域で画像をトリミングして保存
+                        var faces = ParseFaces(output);
+                        int faceCnt = 0;
+                        foreach (var face in faces)
+                        {
+                            using (var faceImage = cap.Clone(new OpenCvSharp.CvRect((int)face.X, (int)face.Y, (int)face.Width, (int)face.Height)))
+                            {
+                                if (System.IO.Directory.Exists(dirName) == false)
+                                {
+                                    System.IO.Directory.CreateDirectory(dirName);
+                                }
+                                faceImage.SaveImage(System.IO.Path.Combine(dirName, capture.PosFrames + "-" + (faceCnt++) + ".png"));
+                            }
+                        }
+                    }
+
+                    // 終了判定
+                    if (capture.FrameCount - 1 <= capture.PosFrames + arg.skipFrames)
+                    {
+                        worker.ReportProgress(100);
+                        e.Result = true;
+                        return;
+                    }
+
+                    capture.PosFrames = capture.PosFrames + arg.skipFrames;
+
+                    // プログレス更新
+                    int progress = (int)(100.0f * capture.PosFrames / capture.FrameCount);
+                    if ( prevProgress != progress)
+                    {
+                        worker.ReportProgress(progress);
+                    }
+                    prevProgress = progress;
+                }
+            }
+
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressLabel.Text = e.ProgressPercentage + "%";
+            progressBar.Value = e.ProgressPercentage;
+
+            setUIEnabled(true);
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled the detection");
+            }
+            else
+            {
+                bool result = (bool)e.Result;
+                if (result)
+                {
+                    MessageBox.Show("The detection succeeded!");
+                }
+                else
+                {
+                    MessageBox.Show("The detection failed..");
+                }
+            }
+
+            setUIEnabled(false);
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.CancelAsync();
+        }
+
+
+        // Pythonスクリプトの実行
+        string RunCPython(string rootDir, string arguments, out bool result)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo psInfo = new System.Diagnostics.ProcessStartInfo();
+
+                psInfo.FileName = "python.exe"; // 実行するファイル
+                psInfo.Arguments = arguments;
+                psInfo.CreateNoWindow = true; // コンソール・ウィンドウを開かない
+                psInfo.UseShellExecute = false; // シェル機能を使用しない
+                psInfo.RedirectStandardOutput = true; // 標準出力をリダイレクト
+                psInfo.WorkingDirectory = System.IO.Path.GetFullPath(rootDir);
+                System.Diagnostics.Process p = System.Diagnostics.Process.Start(psInfo); // アプリの実行開始
+                string output = p.StandardOutput.ReadToEnd(); // 標準出力の読み取り
+
+                result = true;
+                return output;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString() + "\n" + ex.StackTrace);
+                result = false;
+                return "";
+            }
+        }
+
+        // pythonからの出力をパースして、顔領域を抽出
+        List<RectangleF> ParseFaces(string output)
+        {
+            var faces = new List<RectangleF>();
+
+            foreach (var _line in output.Split('\n'))
+            {
+                var line = _line.Trim().Trim('(', ')').Trim();
+                var tkns = line.Split(',')
+                    .Where(tkn => false == string.IsNullOrWhiteSpace(tkn))
+                    .Select(tkn => tkn.Trim())
+                    .ToArray();
+
+                if (tkns.Length != 4)
+                {
+                    continue;
+                }
+
+                float x, y, w, h;
+                if (float.TryParse(tkns[0], out x) &&
+                    float.TryParse(tkns[1], out y) &&
+                    float.TryParse(tkns[2], out w) &&
+                    float.TryParse(tkns[3], out h))
+                {
+                    faces.Add(new RectangleF(x, y, w, h));
+                }
+            }
+
+            return faces;
+        }
+
+        private void outputFolderButton_Click(object sender, EventArgs e)
+        {
+            string path = outputFolderButton.Tag as string;
+            if (path != null && System.IO.Directory.Exists(path))
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+        }
+    }
+}
